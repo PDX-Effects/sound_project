ce795863ecd220a756e5ffdf9deddaec0f4600e1
Adam La Fleur <alafleur938@gmail.com>
2021-05-11 00:30:09 -0700

    frame
    frame

---
+++ src/effects.py
@@ -0,0 +1,12 @@
+class Effects:
+    def chorus(self, data):
+        return 0
+    def flang(self, data):
+        return 0
+    def delay(self, data):
+        return 0
+    def floop(self, data):
+        return 0
+    def clipping(self, data):
+        return 0
+    
