2742b97b6168316c2e6f695a6f5c2f85f1e54ab9
kelsey-debug <kelsey36@pdx.edu>
2021-05-17 16:10:57 -0700

    delay working, changes info.samples to pure byte object NOT in a 1d array

--- src/effects.py
+++ src/effects.py
@@ -1,16 +1,17 @@
 from frame import Frame
-from audioop import add
+from audioop import add #allows us to add byte object audio signals together
 import numpy as np
 
-#TODO: use bytes object itself not 1d array of byte sample
 class Effects:
     def chorus(self, info): 
-      #delay, change freq with filter.
       #3 second delay
+      #http://andrewslotnick.com/posts/audio-delay-with-python.html for buffersize help
       buff_size = info.sampwidth * 3 * int(info.framerate)
       buffer = b'\0' * buff_size # must use b for byte literal class for info.samples
-      mod_signal = info.samples[:-buff_size] #compensate for buffer size
+      mod_signal = info.samples[:-buff_size] 
       info.samples = add(info.samples ,buffer + mod_signal,  info.sampwidth)
+
+      #add LFO
       return info
 
     def flang(self, info):
