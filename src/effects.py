cf8c135d27027c0001cfbd28a4a3aaeede888212
kelsey-debug <kelsey36@pdx.edu>
2021-05-18 18:23:48 -0700

    changed to ms

--- src/effects.py
+++ src/effects.py
@@ -4,14 +4,18 @@ import numpy as np
 
 class Effects:
     def chorus(self, info): 
-      #3 second delay
+      #300 ms delay
       #http://andrewslotnick.com/posts/audio-delay-with-python.html for buffersize help
-      buff_size = info.sampwidth * 3 * int(info.framerate)
+      buff_size = info.sampwidth * 300 * int(info.framerate/1000)
       buffer = b'\0' * buff_size # must use b for byte literal class for info.samples
       mod_signal = info.samples[:-buff_size] 
+
       info.samples = add(info.samples ,buffer + mod_signal,  info.sampwidth)
+#      info.samples = np.sin(bytearray(info.samples))
 
       #add LFO
+
+      #modify amplitude of mod_signal?
       return info
 
     def flang(self, info):
