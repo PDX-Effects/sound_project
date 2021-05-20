e17feb197768d85868fd49c4efcffaebc4aae87d
Adam La Fleur <alafleur938@gmail.com>
2021-05-19 20:25:19 -0700

    Removed threading for now

--- src/main.py
+++ src/main.py
@@ -12,13 +12,6 @@ def play(t_info):
     audio.play_audio(t_info)
 
 
-# https://stackoverflow.com/questions/41371815/how-can-i-stop-my-tkinter-gui-from-freezing-when-i-click-my-button
-def play_background():
-    play_thread = threading.Thread(target=play)
-    play_thread.daemon = True
-    play_thread.start()
-
-
 if __name__ == "__main__":
     info.filename = 'gc.wav'
     info = audio.read_audio(info)
