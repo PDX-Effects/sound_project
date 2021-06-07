a704e34fcbc6b0fbed434d622a82d8428046373d
Maeve Hoffer <dhoffer@pdx.edu>
2021-06-06 18:32:37 -0700

    Implement part of phaser effect.

--- src/effects.py
+++ src/effects.py
@@ -69,7 +69,13 @@ class Effects:
         info.samples = samp
         return info
 
-    def phaser(self, info, dry=0.50, wet=0.50):
+    def phaser(self, info, dry=0.50, wet=0.50, passes = 1):
+        y = info.samples
+        b, a = signal.ellip(4, 0.01, 120, 0.125)
+        for i in range(passes):
+            y += (signal.filtfilt(b, a, y) * 0.5).astype(np.float32)
+
+        info.samples += (y * 0.5)
         return info
 
     def clipping(self, info, percent):
